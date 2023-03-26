1674180f1bc60d48d3b4c3177362ad9b4d372201
Abdul Rehman Nadeem <abdulrehmanadeem14@gmail.com>
2023-03-26 14:37:51 +0500

    testing code with some cases

--- currency.py
+++ currency.py
@@ -1,15 +1,37 @@
 import requests
 
 def get_exchange_rates(currency_main,currency_convert):
-    api = 'tdaZVBN91gy4QOXEsAiZY9PWWMENGZ5p'
-    url = f'https://api.exchangeratesapi.io/live?base={currency_main}&symbol={currency_convert}'
-    response = requests.get(url,headers={'Authorization' : f'Bearer {api}'})
+    headers= {
+     "apikey": "tdaZVBN91gy4QOXEsAiZY9PWWMENGZ5p"
+    }
+    payload ={}
+    url = f"https://api.apilayer.com/exchangerates_data/latest?base={currency_main}&symbol={currency_convert}"
+    response = requests.request("GET", url, headers=headers, data = payload)
 
     if response.status_code == 200:
         data = response.json()
+        print(data)
         return data['rates'][currency_convert]
     else:
         raise Exception(f"Error getting exchange rates: {response.status_code}!")
     
 def calculate_profit(amount,buy,sell,exchange_rate):
+    if buy == sell:
+        return amount,0.0
     
+    buy = amount/exchange_rate
+    sell = amount/exchange_rate
+
+    profit = sell - buy
+
+    return round(profit,2)
+
+
+#Test Case
+amount = int(input("Enter Amount : "))
+buy = input("Enter Currency to buy : ")
+sell = input('Enter Currency to sell : ')
+
+exchange_rate = get_exchange_rates(buy,sell)
+profit = calculate_profit(amount,buy,sell,exchange_rate)
+print(f"Profit from buying {amount} {buy} and selling in {sell} is : {profit}")
